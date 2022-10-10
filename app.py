49ece95ab1574f4a307941dd0b47e768e040b5b7
sauravbaligaconv <109660423+sauravbaligaconv@users.noreply.github.com>
2022-10-10 15:22:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template,request,redirect
 import os
 import time
 import pandas as pd
+from datetime import datetime ,timedelta
 
 app = Flask(__name__)
 IMG_FOLDER = os.path.join('static', 'images')
@@ -26,13 +27,13 @@ def home():
 
 @app.route("/static/images/<user>",methods=['GET',"POST"])
 def Display_IMG1(user):
-    start=time.localtime()
+    start=time.time()
     mail=pd.read_csv('static/csv files/mail_track.csv')
     #print(start)
-    #a=time.gmtime(start)
-    print(time.strftime("%H:%M:%S", start))
+    a=time.gmtime(start)+ timedelta(seconds=0, minutes=30, hours=5)
+    print(time.strftime("%H:%M:%S", a))
     print('mail opened by ',user)
-    mail=mail.append({'UserName':user,'Time':time.strftime("%H:%M:%S", start)},ignore_index=True)
+    mail=mail.append({'UserName':user,'Time':time.strftime("%H:%M:%S", a)},ignore_index=True)
     print(mail)
     mail.to_csv('static/csv files/mail_track.csv',mode='a',index=False,header=False)
     return redirect('https://lh3.googleusercontent.com/p/AF1QipMYpbypAsagW1iih-6hinCGdwiDfZIl7R5R3P8k=w1080-h608-p-no-v0')
