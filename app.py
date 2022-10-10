217433e5ad3d81e0bd26656aff7222545c5d0308
sauravbaligaconv <109660423+sauravbaligaconv@users.noreply.github.com>
2022-10-10 15:28:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,9 @@ def home():
 def Display_IMG1(user):
     start=time.time()
     mail=pd.read_csv('static/csv files/mail_track.csv')
-    #print(start)
+    print(start)
     a=time.gmtime(start)
+    a=a+(5*60)+30
     print(a)
     print(time.strftime("%H:%M:%S", a))
     print('mail opened by ',user)
