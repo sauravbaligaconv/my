ea98d8c27612da045736300bc8ef33a661122246
sauravbaligaconv <109660423+sauravbaligaconv@users.noreply.github.com>
2022-10-20 15:08:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ IMG_FOLDER = os.path.join('static', 'images')
 app.config['UPLOAD_FOLDER'] = IMG_FOLDER
 DATABASE_URL = os.environ['DATABASE_URL']
 print(DATABASE_URL)
-
+con = psycopg2.connect(DATABASE_URL)
 @app.route("/",methods=['GET'])
 @app.route("/home",methods=['GET'])
 def home():
@@ -44,6 +44,10 @@ def Display_IMG1(user):
     #print(mail)
     mail.to_csv('static/csv files/mail_track.csv',mode='a',index=False,header=False)
     print(mail1)
+    cursor = con.cursor()
+    statement='INSERT INTO'+' '+table_name+' '+'(user_name,time)'+' '+'VALUES'+' '+'('+user+','+time.strftime("%H:%M:%S", a)+')'
+    print(statement)
+    #cursor.execute('''INSERT INTO mail_data(user_name, time) VALUES ('Ramya', 'Rama priya', 27, 'F', 9000)''')
     #mail1.to_sql('mail_data', con = engine, if_exists='append')
     return redirect('https://lh3.googleusercontent.com/p/AF1QipMYpbypAsagW1iih-6hinCGdwiDfZIl7R5R3P8k=w1080-h608-p-no-v0')
 
