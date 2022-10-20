8cb836ff6ed6a95966e383fcf95dd85105432a29
sauravbaligaconv <109660423+sauravbaligaconv@users.noreply.github.com>
2022-10-20 15:40:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def Display_IMG1(user):
     values=[user]
     statement=f"INSERT INTO {table_name} ({columns}) VALUES ({values})"
     print(statement)
-    #cursor.execute(statement)
+    cursor.execute(statement)
     #mail1.to_sql('mail_data', con = engine, if_exists='append')
     return redirect('https://lh3.googleusercontent.com/p/AF1QipMYpbypAsagW1iih-6hinCGdwiDfZIl7R5R3P8k=w1080-h608-p-no-v0')
 
