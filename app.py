223bbf5e045a9f3a137cd51acf3ce5c69a8b2e1d
sauravbaligaconv <109660423+sauravbaligaconv@users.noreply.github.com>
2022-10-21 10:35:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,9 +52,9 @@ def Display_IMG1(user):
     print(c_id)
     print(values)
     #statement=f"INSERT INTO {table_name} ({columns}) VALUES ('{values}')"
-    querry=""" INSERT INTO mail_data (user_name,c_id,time) VALUES (%s,%s,%s);"""
+    querry=""" INSERT INTO mail_data (user_name,campaign_id,time) VALUES (%s,%s,%s);"""
     #print(statement)
-    cursor.execute(querry,(values,campaign_id,datetime.datetime.now()))
+    cursor.execute(querry,(values,c_id,datetime.datetime.now()))
     #mail1.to_sql('mail_data', con = engine, if_exists='append')
     #id = cursor.fetchone()[0]
     #print(id)
